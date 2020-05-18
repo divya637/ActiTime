4cd99c2825a567ddca0f0509a0a5ea1eb334cae6
divya637 <divyatomar13@gmail.com>
2020-05-18 16:43:08 +0530

    framework created

---
+++ src/main/java/com/genric/demo.java
@@ -0,0 +1,8 @@
+package com.genric;
+
+public class demo {
+public static  void print()
+{
+	System.out.println("hello this is architectSystem");
+}
+}
---
+++ src/test/java/com/test/DemoTest.java
@@ -0,0 +1,11 @@
+package com.test;
+
+import org.testng.annotations.Test;
+
+public class DemoTest {
+@Test
+public void demo()
+{
+	com.genric.demo.print();
+}
+}
